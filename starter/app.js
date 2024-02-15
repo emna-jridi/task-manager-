d990f4620cfc2e7fbb31ff50e87546a6bbf7e64b
emna-jridi <130873553+emna-jridi@users.noreply.github.com>
2024-02-15 13:07:57 +0100

    Update app.js

--- starter/app.js
+++ starter/app.js
@@ -22,7 +22,7 @@ const start = async()=>{
     try{
 
         await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`server is lestening on port ${port}`))
+        app.listen(port, console.log(`server is listening on port ${port}`))
 
     }catch (error){
         console.log(error)
